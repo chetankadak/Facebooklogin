e15ec170b054a91a42b5cc5469af7ff44be7d286
chetankadak <chetankadak25@gmail.com>
2025-07-21 14:30:42 +0530

    updated

--- src/main/java/Runner/Runnerclass.java
+++ src/main/java/Runner/Runnerclass.java
@@ -5,7 +5,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 
-@CucumberOptions(features = {"features/signup.feature"},
+@CucumberOptions(features = {"features/fblogin.feature"},
                  glue =     {"StepDefinations"},
                  plugin =    { "pretty","html:target/cucumber-reports",
     		     "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
